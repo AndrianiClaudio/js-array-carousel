e5de134cb82128772fc84301f9507422c8449ae7
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-25 16:00:11 +0100

    MILESTONE 1: creazione container e immagini/thumb

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log('js/script.js caricato correttamente');
+/*
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l’immagine grande a sinistra e le thumbnails sulla destra in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico.
+*/
